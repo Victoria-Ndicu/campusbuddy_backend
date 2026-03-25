4fc312abf21841c853bc48701a7fda221c171d62
VvieyAB <ndicuvictoria@gmail.com>
2026-03-26 01:14:10 +0300

    final4

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=Csv())
+ALLOWED_HOSTS = campusbuddybackend-production.up.railway.app
 
 INSTALLED_APPS = [
     "django.contrib.admin",
